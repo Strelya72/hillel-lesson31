9830d0576eb8641e69a865f114a238418adc1a3b
Danil Strelchenya <strelya72@gmail.com>
2019-06-25 21:27:45 +0300

    Function to power a number is implemented.

--- src/com/strelchenyad/hillel/Main.java
+++ src/com/strelchenyad/hillel/Main.java
@@ -3,6 +3,10 @@ package com.strelchenyad.hillel;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        
+    }
+
+    public static int pow(int value, int level) {
+        return (int) Math.pow(value, level);
     }
 }
